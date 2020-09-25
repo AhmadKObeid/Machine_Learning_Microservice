f66208bb2cc37d7a6a9ec613934b830e712d707a
Ahmad Obeid <aobeid@mx>
2020-09-25 22:43:36 +0300

    integrated service with linear model object

--- flask_template_service.py
+++ flask_template_service.py
@@ -1,40 +1,42 @@
+#!/home/aobeid/anaconda3/bin/python
 """
 Import Necessary Libraries 
 """
 from flask import Flask, request
-from flask_caching import Cache
-import csv
+# from flask_caching import Cache
+# import csv
+import pickle
+import numpy as np
+import argparse
 
-"""
-Flask App Initiation and Flask Cache Setup
-"""
-cache = Cache()
+
 app = Flask(__name__)
-app.config["CACHE_TYPE"] = "simple"
-cache.init_app(app)
 
 """
 Functions used in Flask App
 """
-@cache.cached(timeout=3600, key_prefix="thetas")
-def read_coefficient(filename):
-    """this function takes the csv file as input,
-        reads the file, returns the list of
-        coefficients, and caches the data for
-        later use
+def read_arguments():
+    """
+    [summary]
+    
+    reading arguments from console
+    """
+    parser = argparse.ArgumentParser(description='Linear Regression Model')
+    parser.add_argument('path', type=str, help='Model Path')
+    args = parser.parse_args()
+    return args
+
+def get_path(args):
+    """[summary]
+    extracting the model path from the arguments
 
     Args:
-        filename (string): name of file to be read
-
+        args (list): arguments list
     Returns:
-        [List]: the coefficients read from the file
+        [string]: model path
     """
-    with open(filename, "r") as f:
-        data = csv.reader(f)
-        coefficients = []
-        for line in data:
-            coefficients.append([float(x) for x in line])
-        return coefficients
+    path = args.path
+    return path
 
 def get_data():
     """Extracts the data from the post request
@@ -43,48 +45,39 @@ def get_data():
         [List]: List of feature values
     """
     data = []
-    data.append(request.args.get("date"))
-    data.append(request.args.get("house_age"))
-    data.append(request.args.get("station_distance"))
-    data.append(request.args.get("stores"))
-    data.append(request.args.get("latitude"))
-    data.append(request.args.get("longitude"))
+    data.append(float(request.args.get("date")))
+    data.append(float(request.args.get("house_age")))
+    data.append(float(request.args.get("station_distance")))
+    data.append(float(request.args.get("stores")))
+    data.append(float(request.args.get("latitude")))
+    data.append(float(request.args.get("longitude")))
+    
+    return np.array(data).reshape(1,6)
 
-    return data
-
-
-def predicted_price(theta0, thetas, data):
-    """using the hypothesis linear eqation to
-        compute the estamited price bulit by
-        the Linear Model
+def predicted_price(data):
+    """using the loaded linear model object to
+        compute the estamited price.
 
     Args:
-        theta0 (float): the intercept of the linear equation
-        thetas (float): the coefficients of the linear equation (theta1----theta6)
         data (List): the parameters of the linear equation (x1----x6)
 
     Returns:
-        [Float]: the output of the linear equation (estimated price)
+        [Float]: the output of the linear Model Object
     """
+    args = read_arguments()
+    path = get_path(args)
+    loaded_model = pickle.load(open(path, 'rb'))
+    price = loaded_model.predict(data)
 
-    price = 0
-    for index, feature in enumerate(data):
-        price += float(feature) * float(thetas[index])
-
-    price += theta0
-
-    return price
+    return price[0]
 
 
 """
 Flask App Routes
 """
-
-
 @app.route("/")
 def welcome():
     return "Welcome to my Model!"
-
 
 @app.route("/predict", methods=["GET", "POST"])
 def get_predicted_price():
@@ -98,16 +91,12 @@ def get_predicted_price():
     """
 
     if request.method == "POST":
-        coefficients = read_coefficient("lm_parameters.csv")
         data = get_data()
-        thetas = coefficients[0]
-        theta0 = coefficients[1][0]
         return (
             "The house is evaluated at: "
-            + str(predicted_price(theta0, thetas, data))
+            + str(predicted_price(data))
             + " per unit area"
         )
-
 
 """
 Flask App Entry Point (Main)
